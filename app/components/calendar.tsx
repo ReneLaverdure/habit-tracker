30ae269ed25d5c4d620cb03c52672ac6d7c1ce99
ReneLaverdure <renelaverdure@gmail.com>
2026-02-20 19:27:20 +1100

    Basic calendar functionality

---
+++ app/components/calendar.tsx
@@ -0,0 +1,101 @@
+import { useState } from "react";
+
+export default function Cadendar() {
+  console.log("calendar");
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  const [currDate, setCurrDate] = useState(new Date());
+
+  // const monthIdx = currDate.getMonth();
+  const [currMonth, setCurrMonth] = useState(currDate.getMonth());
+  const year = currDate.getFullYear();
+
+  const handleNextMonth = () => {
+    setCurrMonth((prev) => {
+      if (prev === 11) {
+        return 0;
+      }
+      return prev + 1;
+    });
+  };
+  const handlePrevMonth = () => {
+    setCurrMonth((prev) => {
+      if (prev === 0) {
+        return 11;
+      }
+      return prev - 1;
+    });
+  };
+  const getDaysInMonth = (month: number, year: number) => {
+    let date = new Date(year, month, 1);
+    let days = [];
+    while (date.getMonth() === month) {
+      days.push(new Date(date));
+      date.setDate(date.getDate() + 1);
+    }
+    return days;
+  };
+
+  let days = getDaysInMonth(currMonth, year);
+  console.log(days);
+  console.log(months[currMonth]);
+  console.log(days[0].getDay());
+
+  let missingFront = days[0].getDay();
+  console.log(missingFront);
+  let holdingDate = new Date(days[0]);
+  console.log("HOLDIng date", holdingDate);
+
+  while (missingFront) {
+    holdingDate.setDate(holdingDate.getDate() - 1);
+    console.log(holdingDate);
+    days.unshift(new Date(holdingDate));
+    missingFront--;
+  }
+
+  console.log(days);
+
+  return (
+    <div>
+      <header className="flex justify-between">
+        <button onClick={handlePrevMonth}>previous</button>
+        <h1>{months[currMonth]}</h1>
+        <button onClick={handleNextMonth}>next</button>
+      </header>
+
+      <div className="grid grid-cols-7 gap-1">
+        <h4>Sunday</h4>
+        <h4>Monday</h4>
+        <h4>Tuesday</h4>
+        <h4>Wednesday</h4>
+        <h4>Thursday</h4>
+        <h4>Friday</h4>
+        <h4>Saturday</h4>
+
+        {days.map((day, idx) => {
+          if (day.getMonth() !== currMonth) {
+            return (
+              <div key={idx} className="text-gray-600">
+                {day.getDate()}
+              </div>
+            );
+          }
+          return <div key={idx}>{day.getDate()}</div>;
+        })}
+      </div>
+    </div>
+  );
+}
--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -1,10 +1,12 @@
 import logoDark from "./logo-dark.svg";
 import logoLight from "./logo-light.svg";
+import Calendar from "../components/calendar";
 
 export function Welcome() {
   return (
     <main className="flex items-center justify-center pt-16 pb-4">
       <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
+        <Calendar />
         <header className="flex flex-col items-center gap-9">
           <div className="w-[500px] max-w-[100vw] p-4">
             <img
